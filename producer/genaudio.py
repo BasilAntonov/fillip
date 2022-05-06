79cec6f7d751fdb0e5c66a80f1677dda6bb51c76
BasilAntonov <antonov.basil@gmail.com>
2022-05-06 17:18:11 +0300

    processing responses from the server

--- producer/genaudio.py
+++ producer/genaudio.py
@@ -35,14 +35,30 @@ def gen_mono(patterns):
     return answer
 
 
-def gen_file(files):
+def gen_file(files: list, _type: str):
     answer = []
     for i in range(len(files)//2):
-        data = np.hstack((files[2*i], files[2*i+1]))
+        left = files[2*i]
+        right = files[2*i+1]
+        if (_type == 'cut'):
+            size = min(left.size, right.size)
+            if (left.size != size):
+                left = left[0:size]
+            else:
+                right = right[0:size]
+        else:
+            pass #TODO
+            # size = max(left.size, right.size)
+            # if (left.size != size):
+            #     left = np.concatenate(left, left[0:int(size-left.size)])
+            # else:
+            #     right = np.concatenate(right, right[0:int(size-right.size)])
+
+        data = np.hstack((left, right))
         answer.append(data)
 
     if len(files) % 2 == 1:
-        empty = np.full(files[-1].shape[0], 0, dtype=TYPE)
+        empty = np.full(files[-1].size, 0, dtype=TYPE)
         data = np.hstack((files[-1], empty))
         answer.append(data)
 
